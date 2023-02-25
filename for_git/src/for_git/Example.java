025326e96a066280ab165bda742923ab67ac3446
hi <hi@hi-Lenovo-E41-15>
2023-02-25 12:32:23 +0530

    success project

---
+++ for_git/src/for_git/Example.java
@@ -0,0 +1,10 @@
+package for_git;
+
+public class Example {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("welcome to first class");
+	}
+
+}
